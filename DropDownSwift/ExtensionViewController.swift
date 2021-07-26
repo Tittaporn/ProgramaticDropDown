5256ce77be2a3adc31cc18577ca9d35700225e3b
Lee <leemccormicck@Lees-MacBook-Air.local>
2021-07-25 20:21:02 -0500

    finished DropDown Test Project

--- DropDownSwift/ExtensionViewController.swift
+++ DropDownSwift/ExtensionViewController.swift
@@ -11,23 +11,7 @@ import DropDown
 
 extension UIViewController {
     
-    // MARK: - Properties
-    //    var safeArea: UILayoutGuide {
-    //        return self.view.safeAreaLayoutGuide
-    //    }
-    
-    //   let dropDown = DropDown()
-    //    let testArray = ["test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4"]
-    
-    // MARK: - Life Cycle Methods
-    //    override func loadView() {
-    //        super.loadView()
-    //        addAllSubView()
-    //        contraintAllSubViews()
-    //    }
-    
-    func setupDropDown(with dropDown: DropDown, dataSource: [String], textField: UITextField) -> String {
-        var selectedDropDownElement: String = ""
+    func setupDropDown(with dropDown: DropDown, textField: UITextField) {
         let vwDropDown: UIView = {
             let view = UIView()
             view.backgroundColor = .red
@@ -36,94 +20,45 @@ extension UIViewController {
         self.view.addSubview(vwDropDown)
         vwDropDown.anchor(top: textField.bottomAnchor, bottom: nil, leading: textField.leadingAnchor, trailing: textField.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
         dropDown.anchorView = vwDropDown
-        dropDown.dataSource = dataSource
         dropDown.bottomOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height)!)
         dropDown.topOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height)!)
         dropDown.direction = .bottom
+    }
+    
+    func updateDropDownDataSource(with dropDown: DropDown, dataSource: [String]) {
+        dropDown.dataSource = dataSource
+    }
+    
+    func updateTextFieldWithDropDownSelection(with dropDown: DropDown, dataSource: [String], textField: UITextField) {
         dropDown.selectionAction = { (index: Int, item: String) in
             print("\n===================Selected item : \(item) at index : \(index) IN \(#function) ======================\n")
             textField.text = dataSource[index]
-            selectedDropDownElement = dataSource[index]
-            
         }
-        return selectedDropDownElement
-        //  activateDropDownButton()
-       // return dataSource[index]
     }
 }
-//    // MARK: - Helper Fuctions
-//    func addAllSubView() {
-//        self.view.addSubview(vwDropDown)
-//        self.view.addSubview(textLabel)
-//        self.view.addSubview(dropDownButton)
-//        self.view.addSubview(testTextField)
-//    }
-//
-//    // Using Extension UIView ==> anchor() ==> to contraint the subViews
-//    func contraintAllSubViews() {
-//        textLabel.anchor(top: safeArea.topAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 30, paddingBottom: 16, paddingLeft: 16, paddingRight: 16)
-//        dropDownButton.anchor(top: textLabel.bottomAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 30, paddingBottom: 0, paddingLeft: 16, paddingRight: 16)
-//        testTextField.anchor(top: dropDownButton.bottomAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 16, paddingLeft: 16, paddingRight: 16)
-//        vwDropDown.anchor(top: testTextField.bottomAnchor, bottom: nil, leading: testTextField.leadingAnchor, trailing: testTextField.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
-//
-//    }
 
-//    @objc func dropDownButtonTapped(sender: UIButton){
-//        print("\n=================== DropDown Button Tapped IN \(#function) ======================\n")
-//        dropDown.show()
-//    }
-//
-//    func activateDropDownButton() {
-//        self.dropDownButton.addTarget(self, action: #selector(dropDownButtonTapped(sender:)), for: .touchUpInside)
-//    }
-//
-//    // MARK: - Views
-//    let vwDropDown: UIView = {
-//        let view = UIView()
-//        view.backgroundColor = .red
-//        return view
-//    }()
-//
-//
-//
-//    let dropDownButton: UIButton = {
-//        let button = UIButton()
-//        button.setTitle("Drop Down", for: .normal)
-//        button.backgroundColor = .red
-//        return button
-//    }()
-//
-//    let testTextField: UITextField = {
-//        let textField = UITextField()
-//        textField.text = "Test Text Field"
-//        textField.backgroundColor = .yellow
-//        return textField
-//    }()
-//}
-//
-//// MARK: - Anchor
-//extension UIView {
-//    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil) {
-//        self.translatesAutoresizingMaskIntoConstraints = false
-//        if let top = top {
-//            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-//        }
-//        if let leading = leading {
-//            leadingAnchor.constraint(equalTo: leading, constant: paddingLeft).isActive = true
-//        }
-//        if let bottom = bottom {
-//            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
-//        }
-//        if let trailing = trailing {
-//            trailingAnchor.constraint(equalTo: trailing, constant: -paddingRight).isActive = true
-//        }
-//        if let width = width {
-//            widthAnchor.constraint(equalToConstant: width).isActive = true
-//        }
-//        if let height = height {
-//            heightAnchor.constraint(equalToConstant: height).isActive = true
-//        }
-//    }
-//}
-//
-//
+// MARK: - Anchor
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: paddingLeft).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -paddingRight).isActive = true
+        }
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
+
--- DropDownSwift/ViewController.swift
+++ DropDownSwift/ViewController.swift
@@ -14,9 +14,17 @@ class ViewController: UIViewController {
     var safeArea: UILayoutGuide {
         return self.view.safeAreaLayoutGuide
     }
-    
-    let drowDown = DropDown()
+    let dropDown = DropDown()
+    let testExtensionDropDown = DropDown()
     let testArray = ["test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4","test1","test2","test3","test4"]
+    let testExtensionDataSource = ["Lee", "Donny", "Lyn", "New", "Night"]
+    var dataSource: [String] = [] {
+        didSet {
+            updateDropDownDataSource(with: dropDown, dataSource: dataSource)
+            updateDropDownDataSource(with: testExtensionDropDown, dataSource: dataSource)
+            
+        }
+    }
     
     // MARK: - Life Cycle Methods
     override func loadView() {
@@ -28,25 +36,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         activateDropDownButton()
-        drowDown.anchorView = vwDropDown
-        drowDown.dataSource = testArray
-        drowDown.bottomOffset = CGPoint(x: 0, y: (drowDown.anchorView?.plainView.bounds.height)!)
-        drowDown.topOffset = CGPoint(x: 0, y: (drowDown.anchorView?.plainView.bounds.height)!)
-        drowDown.direction = .bottom
-        drowDown.selectionAction = { (index: Int, item: String) in
-            print("\n===================Selected item : \(item) at index : \(index) IN \(#function) ======================\n")
-            self.textLabel.text = self.testArray[index]
-            self.testTextField.text = self.testArray[index]
-        }
+        //        dropDown.anchorView = vwDropDown
+        //        dropDown.dataSource = testArray
+        //        dropDown.bottomOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height)!)
+        //        dropDown.topOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height)!)
+        //        dropDown.direction = .bottom
+        //        dropDown.selectionAction = { (index: Int, item: String) in
+        //            print("\n===================Selected item : \(item) at index : \(index) IN \(#function) ======================\n")
+        //            self.textLabel.text = self.testArray[index]
+        //            self.testTextField.text = self.testArray[index]
+        //        }
+        testExtensionactivateDropDownButton()
+        setupDropDown(with: testExtensionDropDown, textField: testExtensionTextField)
+        setupDropDown(with: dropDown, textField: testTextField)
         
     }
     
     // MARK: - Helper Fuctions
     func addAllSubView() {
-        self.view.addSubview(vwDropDown)
+        // self.view.addSubview(vwDropDown)
         self.view.addSubview(textLabel)
         self.view.addSubview(dropDownButton)
         self.view.addSubview(testTextField)
+        self.view.addSubview(testExtensionTextField)
+        self.view.addSubview(testExtensionDropDownButton)
     }
     
     // Using Extension UIView ==> anchor() ==> to contraint the subViews
@@ -54,29 +67,45 @@ class ViewController: UIViewController {
         textLabel.anchor(top: safeArea.topAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 30, paddingBottom: 16, paddingLeft: 16, paddingRight: 16)
         dropDownButton.anchor(top: textLabel.bottomAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 30, paddingBottom: 0, paddingLeft: 16, paddingRight: 16)
         testTextField.anchor(top: dropDownButton.bottomAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 16, paddingLeft: 16, paddingRight: 16)
-        vwDropDown.anchor(top: testTextField.bottomAnchor, bottom: nil, leading: testTextField.leadingAnchor, trailing: testTextField.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
-        
+        //vwDropDown.anchor(top: testTextField.bottomAnchor, bottom: nil, leading: testTextField.leadingAnchor, trailing: testTextField.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
+        testExtensionTextField.anchor(top: testTextField.bottomAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 16, paddingBottom: 16, paddingLeft: 16, paddingRight: 16)
+        testExtensionDropDownButton.anchor(top: testExtensionTextField.bottomAnchor, bottom: nil, leading: testExtensionTextField.leadingAnchor, trailing: testExtensionTextField.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
     }
     
+    func  testExtensionactivateDropDownButton() {
+        self.testExtensionDropDownButton.addTarget(self, action: #selector(testExtensiondropDownButtonTapped(sender:)), for: .touchUpInside)
+    }
+    
+    func activateDropDownButton() {
+        self.dropDownButton.addTarget(self, action: #selector(dropDownButtonTapped(sender:)), for: .touchUpInside)
+    }
+    
+    // MARK: - Actions
     @objc func dropDownButtonTapped(sender: UIButton){
         print("\n=================== DropDown Button Tapped IN \(#function) ======================\n")
-        drowDown.show()
+        dataSource = testArray
+        dropDown.show()
+        updateTextFieldWithDropDownSelection(with: dropDown, dataSource: dataSource, textField: testTextField)
     }
-     
-     func activateDropDownButton() {
-         self.dropDownButton.addTarget(self, action: #selector(dropDownButtonTapped(sender:)), for: .touchUpInside)
-     }
+    
+    @objc func  testExtensiondropDownButtonTapped(sender: UIButton){
+        print("\n=================== testExtensiondropDownButtonTapped Button Tapped IN \(#function) ======================\n")
+        //testExtensionDropDown.show()
+        dataSource = testExtensionDataSource
+        testExtensionDropDown.show()
+        updateTextFieldWithDropDownSelection(with: testExtensionDropDown, dataSource: dataSource, textField: testExtensionTextField)
+    }
     
     // MARK: - Views
-    let vwDropDown: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
+    //    let vwDropDown: UIView = {
+    //        let view = UIView()
+    //        view.backgroundColor = .red
+    //        return view
+    //    }()
     
     let textLabel: UILabel = {
         let label = UILabel()
-        label.text = "Hello World"
+        label.text = "Hello World ! Programatic DropDrop..."
         return label
     }()
     
@@ -93,30 +122,19 @@ class ViewController: UIViewController {
         textField.backgroundColor = .yellow
         return textField
     }()
-}
-
-// MARK: - Anchor
-extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat? = nil, height: CGFloat? = nil) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        if let top = top {
-            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-        }
-        if let leading = leading {
-            leadingAnchor.constraint(equalTo: leading, constant: paddingLeft).isActive = true
-        }
-        if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
-        }
-        if let trailing = trailing {
-            trailingAnchor.constraint(equalTo: trailing, constant: -paddingRight).isActive = true
-        }
-        if let width = width {
-            widthAnchor.constraint(equalToConstant: width).isActive = true
-        }
-        if let height = height {
-            heightAnchor.constraint(equalToConstant: height).isActive = true
-        }
-    }
+    
+    let testExtensionDropDownButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Extension Drop Down", for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+    
+    let testExtensionTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "Extension Text Field"
+        textField.backgroundColor = .yellow
+        return textField
+    }()
 }
 
